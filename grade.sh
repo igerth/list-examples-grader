d79170e01c967bef770fe29905321389050871a2
igerth <122569790+igerth@users.noreply.github.com>
2023-03-02 13:18:35 -0800

    changes

--- grade.sh
+++ grade.sh
@@ -4,7 +4,32 @@ rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
-cp student-submission/ListExamples.java ./
-javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+if [[ -e student-submission/ListExamples.java ]]
+then
+  echo "File found"
+  cp student-submission/ListExamples.java ./
+else
+  echo "File not found"
+  exit 1
+fi
+
+javac -cp $CPATH *.java 2>javac-errs.txt
+
+if [[ $? -eq 0 ]]
+then 
+  echo "Compiled successfully"
+else
+  echo "Compiled unsuccessfully"
+  exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples >out.txt
+
+if [[ $? -eq 0 ]]
+then 
+  echo "Pass"
+else
+  echo "Fail"
+fi
+
 
